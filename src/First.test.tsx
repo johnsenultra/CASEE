effd35664ec231d80f5f0cc83a788e5f7630d4d5
johnsenultra <johnsenultra446@gmail.com>
2025-12-23 17:33:09 +0800

    Add jest dependencies with typescript support and configure for unit test

--- src/First.test.tsx
+++ src/First.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { First } from './First';
 
-test('it renders a heading', () => {
-  render(<First />);
-  const heading = screen.getByRole('heading'); // Finds any heading element (h1, h2, etc.)
-  expect(heading).toBeInTheDocument();
+describe('First test', () => {
+  it('It should render component', () => {
+    render(<First />)
+    expect(true).toBeTruthy()
+
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument()
+  });
 });

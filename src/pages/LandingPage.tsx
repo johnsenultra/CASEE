0e4dc6e16ad26cdbd34fe0f015929d716051983c
johnsenultra <johnsenultra446@gmail.com>
2026-01-09 17:36:03 +0800

    feat: add LandingPage component with basic structure

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+export default function LandingPage() {
+  return (
+    <main className="grow">
+      <div className="mx-auto mt-25 max-w-screen">
+        <div>
+          <h1>Hello</h1>
+          <h1>World</h1>
+        </div>
+      </div>
+    </main>
+  )
+}

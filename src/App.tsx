29cd3c73b8cda137bc648fcaaaca170c100aeffb
johnsenultra <johnsenultra446@gmail.com>
2026-01-09 17:36:14 +0800

    feat: restructure App component to include routing and authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { First } from "./First"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AuthProvider } from "./context/AuthProvider"
+import LandingPage from "./pages/LandingPage"
+import AuthLayout from "./layouts/AuthLayout"
+import DashboarLayout from "./layouts/DashboardLayout"
+import NotFound from "./pages/NotFound"
 
 function App() {
 
   return (
-    <div>
-      <First />
-    </div>
+    // Wrap the app with auth provider to provide authencation context
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <LandingPage /> } />
+          <Route path="/auth" element={ <AuthLayout /> }>
+            {/* Public routes */}
+          </Route>
+
+          <Route path="/" element={<DashboarLayout />}>
+            {/* Private routes */}
+          </Route>
+          
+          {/* Catch not found pages */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 

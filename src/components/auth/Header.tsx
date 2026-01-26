a90deb2b18f7c3e2ec4520c466c5f87828c36a28
johnsenultra <johnsenultra446@gmail.com>
2026-01-26 16:44:19 +0800

    feat: created the whole header and navigation for the landing page, auth navigation and page navigation

---
+++ src/components/auth/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import AuthLogo from "./AuthLogo";
+
+export default function Header() {
+  return (
+    <div>
+      <header className="py-3 text-white bg-[#764ba2]">
+        <nav className="mx-auto flex max-w-7xl flex-wrap items-center justify-between px-3 md:px-6 lg-px-12">
+          {/* Logo here */}
+          <div>
+            <AuthLogo />
+          </div>
+          {/* Navigation links */}
+          <div className="font-medium text-[1rem] md:text-lg">
+            <ul className="flex flex-row gap-7">
+              <li>How it works</li>
+              <li>Contact</li>
+              <Link to="signin">Login</Link>
+              <Link to="signup">
+                <span className="cursor-pointer rounded-xl bg-black px-4 py-2 text-sm hover:bg-gray-800 md:text-base">Signup</span>
+              </Link>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}

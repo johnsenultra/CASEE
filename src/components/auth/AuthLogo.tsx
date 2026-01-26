7c9b61ec522e813d7caad0006d27c6055133c9ce
johnsenultra <johnsenultra446@gmail.com>
2026-01-26 16:43:19 +0800

    added an mock logo place holder

---
+++ src/components/auth/AuthLogo.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function AuthLogo() {
+  return (
+    <Link to="/">
+      <p className="text-xl font-medium">CSAEE</p>
+    </Link>
+  )
+}

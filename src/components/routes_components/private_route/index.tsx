b8541c2c43eab93b3968e4a691a5939421c458a6
johnsenultra <johnsenultra446@gmail.com>
2026-02-02 17:03:33 +0800

    fix: improve formatting and readability in PrivateRoute component

--- src/components/routes_components/private_route/index.tsx
+++ src/components/routes_components/private_route/index.tsx
@@ -3,26 +3,30 @@ import { useAuth } from "@/context/AuthProvider";
 import type React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function PrivateRoute({ children }: { children: React.JSX.Element }) {
+export default function PrivateRoute({
+  children,
+}: {
+  children: React.JSX.Element;
+}) {
   const { session, loading } = useAuth();
 
   if (loading) {
     return (
-      <div className="flex item-center space-x-4">
+      <div className="item-center flex space-x-4">
         <Skeleton className="h-12 w-12 rounded-full" />
         <div className="space-y-2">
           <Skeleton className="h-4 w-[62.5]" />
           <Skeleton className="h-4 w-[50]" />
         </div>
       </div>
-    )
+    );
   }
 
   // if not logged in, redirect to signin
-  if(!session) {
-    return <Navigate to={"/auth/signin"} replace />
+  if (!session) {
+    return <Navigate to={"/auth/signin"} replace />;
   }
 
   // if logged in rended the children
-  return <div>{children}</div>
+  return <div>{children}</div>;
 }

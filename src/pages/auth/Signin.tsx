9a82144fe02c1ebcfaa171fd15fb47441f7c744b
johnsenultra <johnsenultra446@gmail.com>
2026-02-03 18:19:47 +0800

    feat: Implement initial signin and signup pages with dedicated forms, public routing, and updated header navigation.

--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,7 +1,32 @@
+import SigninForm from "@/components/auth/SigninForm";
+
 export default function Signin() {
   return (
-    <>
-      
-    </>
-  )
+    <main className="grow">
+      <div className="flex min-h-screen">
+        <div className="hidden w-1/2 items-center justify-center p-12 lg:flex">
+          <div></div>
+          <div></div>
+          <div className="relative max-w-md">
+            <blockquote>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </blockquote>
+            <p>-Jansennn</p>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-center lg:w-1/2">
+          <div className="w-full max-w-md px-4">
+            <div className="mb-8 text-center">
+              <h2 className="text-3xl font-bold text-gray-700">Welcome Back</h2>
+              <p className="mt-2 text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+            {/* Singin Form */}
+            <SigninForm />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }

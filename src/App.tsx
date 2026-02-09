c8784d897e53ae06baa22e9ad35f7736ccc6ca59
johnsenultra <johnsenultra446@gmail.com>
2026-02-09 18:15:22 +0800

    feat: Implement public routes for landing, sign-in, and sign-up pages with dedicated layouts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { AuthProvider } from "./context/AuthProvider"
-import LandingPage from "./pages/LandingPage"
-import AuthLayout from "./layouts/AuthLayout"
-import DashboarLayout from "./layouts/DashboardLayout"
-import NotFound from "./pages/NotFound"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import LandingPage from "./pages/LandingPage";
+import AuthLayout from "./layouts/AuthLayout";
+import DashboarLayout from "./layouts/DashboardLayout";
+import NotFound from "./pages/NotFound";
+import PublicRoute from "./components/routes_components/public_route";
+import Signin from "./pages/auth/Signin";
+import Signup from "./pages/auth/Signup";
+import PublicLayout from "./layouts/PublicLayout";
 
 function App() {
-
   return (
     // Wrap the app with auth provider to provide authencation context
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <LandingPage /> } />
-          <Route path="/auth" element={ <AuthLayout /> }>
-            {/* Public routes */}
+          {/* Landing / Marketing pages */}
+          <Route element={<PublicLayout />}>
+            <Route
+              index
+              element={
+                <PublicRoute>
+                  <LandingPage />
+                </PublicRoute>
+              }
+            />
+          </Route>
+          {/* Public routes / auth pages */}
+          <Route element={<AuthLayout />}>
+            <Route
+              path="/signin"
+              element={
+                <PublicRoute>
+                  <Signin />
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="signup"
+              element={
+                <PublicRoute>
+                  <Signup />
+                </PublicRoute>
+              }
+            />
           </Route>
 
-          <Route path="/" element={<DashboarLayout />}>
-            {/* Private routes */}
-          </Route>
-          
+          <Route element={<DashboarLayout />}>{/* Private routes */}</Route>
+
           {/* Catch not found pages */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;

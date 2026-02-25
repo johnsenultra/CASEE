b4db5d85cf56bcc90c39f125d98c9ea6a3a3a4ed
johnsenultra <johnsenultra446@gmail.com>
2026-02-25 16:37:22 +0800

    feat: Implement private routes for dashboard components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import LandingPage from "./pages/LandingPage";
 import AuthLayout from "./layouts/AuthLayout";
-import DashboarLayout from "./layouts/DashboardLayout";
 import NotFound from "./pages/NotFound";
 import PublicRoute from "./components/routes_components/public_route";
 import Signin from "./pages/auth/Signin";
 import Signup from "./pages/auth/Signup";
 import PublicLayout from "./layouts/PublicLayout";
+import PrivateRoute from "./components/routes_components/private_route";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Appointments from "./pages/dashboard/Appointments";
+import Students from "./pages/dashboard/Students";
+import Counselors from "./pages/dashboard/Counselors";
+import Schedule from "./pages/dashboard/Schedule";
+import Notifications from "./pages/dashboard/Notifications";
+import Settings from "./pages/dashboard/Settings";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
@@ -47,7 +55,64 @@ function App() {
             />
           </Route>
 
-          <Route element={<DashboarLayout />}>{/* Private routes */}</Route>
+          <Route path="/dashboard" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="appointments"
+              element={
+                <PrivateRoute>
+                  <Appointments />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="students"
+              element={
+                <PrivateRoute>
+                  <Students />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="counselors"
+              element={
+                <PrivateRoute>
+                  <Counselors />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="schedule"
+              element={
+                <PrivateRoute>
+                  <Schedule />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="notifications"
+              element={
+                <PrivateRoute>
+                  <Notifications />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="settings"
+              element={
+                <PrivateRoute>
+                  <Settings />
+                </PrivateRoute>
+              }
+            />
+          </Route>
 
           {/* Catch not found pages */}
           <Route path="*" element={<NotFound />} />
